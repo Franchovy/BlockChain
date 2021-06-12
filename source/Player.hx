f43ae9bb39834c2628a8944a216e36f66a574058
Maxime <maxrofra@gmail.com>
2021-06-12 16:23:20 +0200

    Updated player movement to be "boost"-based

--- source/Player.hx
+++ source/Player.hx
@@ -23,6 +23,11 @@ class Player extends FlxSprite
 	var left:FlxActionDigital;
 	var right:FlxActionDigital;
 
+	var hold_up:FlxActionDigital;
+	var hold_down:FlxActionDigital;
+	var hold_left:FlxActionDigital;
+	var hold_right:FlxActionDigital;
+
 	var targetVelX:Float = 0;
 	var targetVelY:Float = 0;
 
@@ -36,9 +41,6 @@ class Player extends FlxSprite
 		// Make the player graphic.
 		makeGraphic(TILE_SIZE, TILE_SIZE, FlxColor.WHITE);
 
-		maxVelocity.x = MAX_SPEED;
-		maxVelocity.y = MAX_SPEED;
-
 		addInputs();
 	}
 
@@ -51,62 +53,94 @@ class Player extends FlxSprite
 		left = new FlxActionDigital();
 		right = new FlxActionDigital();
 
-		up.addKey(W, PRESSED);
-		down.addKey(S, PRESSED);
-		left.addKey(A, PRESSED);
-		right.addKey(D, PRESSED);
+		hold_up = new FlxActionDigital();
+		hold_down = new FlxActionDigital();
+		hold_left = new FlxActionDigital();
+		hold_right = new FlxActionDigital();
+
+		up.addKey(W, JUST_PRESSED);
+		down.addKey(S, JUST_PRESSED);
+		left.addKey(A, JUST_PRESSED);
+		right.addKey(D, JUST_PRESSED);
+
+		hold_up.addKey(W, PRESSED);
+		hold_down.addKey(S, PRESSED);
+		hold_left.addKey(A, PRESSED);
+		hold_right.addKey(D, PRESSED);
 
 		if (actions == null)
 			actions = FlxG.inputs.add(new FlxActionManager());
-		actions.addActions([up, down, left, right]);
+		actions.addActions([up, down, left, right, hold_up, hold_down, hold_right, hold_left]);
 	}
+
+	static inline var ACC_DRAG = 0.9;
+	static inline var VEL_DRAG = 0.9;
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 
-		acceleration.x = 0;
-		acceleration.y = 0;
+		acceleration.x = -acceleration.x * ACC_DRAG;
+		acceleration.y = -acceleration.y * ACC_DRAG;
+
+		velocity.x = velocity.x * VEL_DRAG;
+		velocity.y = velocity.y * VEL_DRAG;
 
 		updateDigital();
+
+		cooldown_elapsed += elapsed;
+
+		if (cooldown_elapsed > BOOST_COOLDOWN)
+		{
+			if (hold_up.triggered && !hold_down.triggered)
+			{
+				acceleration.y = -AUTO_BOOST_SPEED;
+				cooldown_elapsed = 0.0;
+			}
+			else if (hold_down.triggered && !hold_up.triggered)
+			{
+				acceleration.y = AUTO_BOOST_SPEED;
+				cooldown_elapsed = 0.0;
+			}
+
+			if (hold_left.triggered && !hold_right.triggered)
+			{
+				acceleration.x = -AUTO_BOOST_SPEED;
+				cooldown_elapsed = 0.0;
+			}
+			else if (hold_right.triggered && !hold_left.triggered)
+			{
+				acceleration.x = AUTO_BOOST_SPEED;
+				cooldown_elapsed = 0.0;
+			}
+		}
 	}
 
-	function updateDigital():Void
+	static inline var BOOST_SPEED = 40000;
+	static inline var AUTO_BOOST_SPEED = 60000;
+
+	static inline var BOOST_COOLDOWN = 0.3;
+
+	var cooldown_elapsed = 0.0;
+
+	function updateDigital()
 	{
-		shouldMultiplyDiagonal = (down.triggered != up.triggered) && (left.triggered != right.triggered);
-
-		var currentAcceleration = ACCELERATION * (shouldMultiplyDiagonal ? DIAGONAL_MULTIPLER : 1.0);
-
-		if (down.triggered != up.triggered)
+		if (up.triggered && !down.triggered)
 		{
-			if (down.triggered)
-			{
-				acceleration.y = currentAcceleration;
-			}
-			else if (up.triggered)
-			{
-				acceleration.y = -currentAcceleration;
-			}
+			acceleration.y = -BOOST_SPEED;
 		}
-		else if (!down.triggered && !up.triggered)
+		else if (down.triggered && !up.triggered)
 		{
-			acceleration.y = -velocity.y * BRAKE_MULTIPLIER;
+			acceleration.y = BOOST_SPEED;
 		}
 
-		if (left.triggered != right.triggered)
+		if (right.triggered && !left.triggered)
 		{
-			if (left.triggered)
-			{
-				acceleration.x = -currentAcceleration;
-			}
-			else if (right.triggered && !left.triggered)
-			{
-				acceleration.x = currentAcceleration;
-			}
+			acceleration.x = BOOST_SPEED;
 		}
-		else if (!right.triggered && !left.triggered)
+		else if (left.triggered && !right.triggered)
 		{
-			acceleration.x = -velocity.x * BRAKE_MULTIPLIER;
+			acceleration.x = -BOOST_SPEED;
 		}
 	}
 }
