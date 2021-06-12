92737857c32422099b811218d81aa4e573b28a76
Maxime <maxrofra@gmail.com>
2021-06-12 13:44:29 +0200

    Spawn obstacles on a regular interval

--- source/Obstacle.hx
+++ source/Obstacle.hx
@@ -12,8 +12,13 @@ class Obstacle extends FlxSprite
 
 	public function new()
 	{
-		var x:Int;
-		var y:Int;
+		super();
+
+		makeGraphic(SIZE, SIZE, FlxColor.BLUE);
+	}
+
+	public function spawn()
+	{
 		var isVertical:Bool = Random.bool();
 
 		if (isVertical)
@@ -29,13 +34,40 @@ class Obstacle extends FlxSprite
 			y = Random.int(0, FlxG.height - SIZE);
 		}
 
-		super(x, y);
-
-		makeGraphic(SIZE, SIZE, FlxColor.BLUE);
-
 		if (isVertical)
 			velocity.y = y > 0 ? -SPEED : SPEED;
 		else
 			velocity.x = x > 0 ? -SPEED : SPEED;
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		trace("Velocity X:");
+		trace(velocity.x);
+		trace("Velocity Y:");
+		trace(velocity.y);
+
+		if (velocity.x > 0 && x > FlxG.width)
+		{
+			kill();
+			trace("Kill right");
+		}
+		else if (velocity.x < 0 && x < SIZE)
+		{
+			kill();
+			trace("Kill left");
+		}
+		else if (velocity.y > 0 && y > FlxG.height)
+		{
+			kill();
+			trace("Kill bottom");
+		}
+		else if (velocity.y < 0 && y < SIZE)
+		{
+			kill();
+			trace("Kill top");
+		}
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,16 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 class PlayState extends FlxState
 {
+	var obstaclesPool:FlxTypedGroup<Obstacle>;
+
+	var player:Player;
+	var enemy:Enemy;
+	var blockchain:FlxTypedGroup<ChainBlock>;
+
 	override public function create()
 	{
 		super.create();
@@ -15,12 +22,42 @@ class PlayState extends FlxState
 		var playerAndEnemy = new PlayerEnemyChain(7);
 		add(playerAndEnemy);
 
-		var obstacle = new Obstacle();
-		add(obstacle);
+		var poolSize = 30;
+		obstaclesPool = new FlxTypedGroup<Obstacle>(poolSize);
+		for (i in 0...poolSize)
+		{
+			var obstacle = new Obstacle();
+			obstacle.kill();
+			obstaclesPool.add(obstacle);
+		}
 	}
+
+	var elapsedSinceLastSpawn:Float = 0;
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		// 1 out of 10 chance of spawning block per second.
+		if (Random.float(0, 10.0) * elapsedSinceLastSpawn > 9)
+		{
+			var obstacle = obstaclesPool.recycle(Obstacle);
+
+			add(obstacle);
+			obstacle.spawn();
+
+			trace("Spawning obstacle");
+
+			trace("X: ");
+			trace(obstacle.x);
+			trace("Y: ");
+			trace(obstacle.y);
+
+			elapsedSinceLastSpawn = 0;
+		}
+		else
+		{
+			elapsedSinceLastSpawn += elapsed;
+		}
 	}
 }
